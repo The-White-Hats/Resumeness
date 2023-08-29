cc2875c8fad1cb0ec6447d9e0b20b9603b22cb5d
Ahmed Aladdin <ahmed.ibrahim035@eng-st.cu.edu.eg>
2023-08-29 12:04:53 +0300

    style the Creators section

--- src/pages/About/sub/Creators/Creators.tsx
+++ src/pages/About/sub/Creators/Creators.tsx
@@ -1,9 +1,21 @@
+import { data } from './data.json';
+import './Creators.css';
 
 const Creators = () => {
   return (
-    <div>
-      
-    </div>
+    <section id="creators">
+      <h2>Who are the Creators?</h2>
+      <div id="creators-container">
+        {data.map(creator => (
+          <div key={creator.id}>
+            <a href={creator.gitHub}>
+              <img src={creator.image} alt={creator.name} />
+            </a>
+            <h3>{creator.name}</h3>
+          </div>
+        ))}
+      </div>
+    </section>
   );
 }
 
