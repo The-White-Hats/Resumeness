4f1080df1d7082689536aa0cd9f36b2e922dd629
Ahmed Hamed <Ahmed.Hamed03@eng-st.cu.edu.eg>
2023-09-07 23:29:46 +0300

    User can be deleted

--- Server/controllers/profile.controller.js
+++ Server/controllers/profile.controller.js
@@ -1,28 +1,41 @@
-import { Resume } from '../models/resume.model.js';
-import { CoverLetter } from '../models/cover-letter.model.js';
-
+import { Resume } from '../models/resume.model.js'
+import { CoverLetter } from '../models/cover-letter.model.js'
 
 const ProfileController = {
     getProfile: async (req, res) => {
-        const user = req.user;
+        const user = req.user
         try {
-            const allResume = await Resume.find({ userID: user._id });
-            const allCoverLetter = await CoverLetter.find({ userID: user._id });
-            return res.status(200).json({ allResume, allCoverLetter });
-        }
-        catch (error) {
-            return res.status(500).json({ error: "An error occurred while getting Profile. Please try again." });
+            const allResume = await Resume.find({ userID: user._id })
+            const allCoverLetter = await CoverLetter.find({ userID: user._id })
+            return res.status(200).json({
+                message: "Here are All user's resumes and cover-letters",
+                allResume,
+                allCoverLetter
+            })
+        } catch (error) {
+            return res
+                .status(500).json({
+                    error: 'An error occurred while getting Profile. Please try again.',
+                })
         }
     },
     deleteProfile: async (req, res) => {
-        const user = req.user;
+        const user = req.user
         try {
-            return res.status(200).json(user);
+            await Resume.deleteMany({ userID: user._id })
+            await CoverLetter.deleteMany({ userID: user._id })
+            await user.deleteOne()
+            return res.status(200).json({
+                message: 'User is successfully deleted with all his/her resumes and cover-letters',
+                user,
+            })
+        } catch (error) {
+            return res
+                .status(500).json({
+                    error: 'An error occurred while deleting Profile. Please try again.',
+                })
         }
-        catch (error) {
-            return res.status(500).json({ error: "An error occurred while deleting Profile. Please try again." });
-        }
-    }
+    },
 }
 
-export default ProfileController;
+export default ProfileController
