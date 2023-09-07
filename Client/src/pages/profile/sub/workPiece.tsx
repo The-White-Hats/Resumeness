3bd92e9c6f764093ae3f16821f5d38681e90053b
Ahmed Aladdin <ahmed.ibrahim035@eng-st.cu.edu.eg>
2023-09-07 16:23:29 +0300

    all the setters I need

--- Client/src/pages/profile/sub/workPiece.tsx
+++ Client/src/pages/profile/sub/workPiece.tsx
@@ -5,7 +5,40 @@ import * as form from "./../../../slices/formReducer";
 import { updateColor } from '../../../slices/colorReducer';
 import { Download } from '../../resume/sub/download-button/download-button';
 
-const WorkPiece = ({ userWork, setUserWork, work, light, dark } : { userWork:any, setUserWork:any, work: any, light: string, dark: string }) => {
+  /// All the setters functions I am gonna need
+  const SetResumeId = (id: string) => dispatch(form.setResumeId(id));
+  const SetCoverLetterId = (id: string) => dispatch(form.setCoverLetterId(id));
+  const SetFileName = (title: string) => dispatch(form.setFileName(title));
+  const SetFirstName = (fName: string) => dispatch(form.setFirstName(fName));
+  const SetLastName = (lName: string) => dispatch(form.setLastName(lName));
+  const SetEmail = (email: string) => dispatch(form.setEmail(email));
+  const SetTitle = (title: string) => dispatch(form.setTitle(title));
+  const SetAddress = (address: string) => dispatch(form.setAddress(address));
+  const SetPhone = (phone: string) => dispatch(form.setPhone(phone));
+  const SetLinkedInURL = (InURL: string) =>
+    dispatch(form.setLinkedInURL(InURL));
+  const SetPortfolioURL = (PortURL: string) =>
+    dispatch(form.setPortfolioURL(PortURL));
+  const SetProfessionalSummary = (PortURL: string) =>
+    dispatch(form.setProfessionalSummary(PortURL));
+  const SetExperience = (experience: form.experience[]) =>
+    dispatch(form.setExperience(experience));
+  const SetEducation = (education: form.education[]) =>
+    dispatch(form.setEducation(education));
+  const SetSkills = (skills: form.skill[]) => dispatch(form.setSkills(skills));
+  const SetLanguages = (languages: form.language[]) =>
+    dispatch(form.setLanguages(languages));
+  const SetCertifications = (certifications: form.certification[]) =>
+    dispatch(form.setCertifications(certifications));
+  const SetInterests = (interests: form.interest[]) =>
+    dispatch(form.setInterests(interests));
+  const SetCompany = (company: string) => dispatch(form.setCompany(company));
+  const SetHiringManager = (hiringManager: string) =>
+    dispatch(form.setHiringManager(hiringManager));
+  const SetLetterDetails = (letterDetails: string) =>
+    dispatch(form.setLetterDetails(letterDetails));
+  const SetColor = (color: string) => dispatch(updateColor(color));
+  /////////////////////////////////////////////////////////////////////////////
   const buttonsCard = useRef<HTMLDivElement>(null); 
 
   const handleClickingOptions = () => {
