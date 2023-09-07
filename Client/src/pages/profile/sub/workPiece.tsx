a833d76e23609ae112bd6af3ca36e0134ede1074
Ahmed Aladdin <ahmed.ibrahim035@eng-st.cu.edu.eg>
2023-09-07 12:15:45 +0300

    work options enhanced

--- Client/src/pages/profile/sub/workPiece.tsx
+++ Client/src/pages/profile/sub/workPiece.tsx
@@ -1,5 +1,17 @@
+import { useRef } from 'react';
 
 const WorkPiece = ({ userWork, setUserWork, work, light, dark } : { userWork:any, setUserWork:any, work: any, light: string, dark: string }) => {
+  const buttonsCard = useRef<HTMLDivElement>(null); 
+
+  const handleClickingOptions = () => {
+    if(buttonsCard.current)
+    if(buttonsCard.current.classList.contains('hide')) buttonsCard.current.classList.remove('hide');
+    else buttonsCard.current.classList.add('hide');
+  }
+
+  const focusOut = () => {
+    if(buttonsCard.current) buttonsCard.current.classList.add('hide');
+  }
 
   const DeletePieceOfWork = async (work : any) => {
     const id = work._id;
@@ -20,10 +32,10 @@ const WorkPiece = ({ userWork, setUserWork, work, light, dark } : { userWork:any
         <h2><span>Title:</span> {work.title}</h2>
         <p style={{color: dark}}>{work.type}</p>
       </div>
-      <button className="options-button">
+      <button className="options-button" onClick={handleClickingOptions} onBlur={focusOut}>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 512" fill={dark}><path d="M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z"/></svg>
       </button>
-      <div className="buttons-card" style={{borderColor: dark}}>
+      <div ref={buttonsCard} className="buttons-card hide" style={{borderColor: dark}}>
         <button className="edit-card">Edit</button>
         <button className="edit-card">download</button>
         <button className="delete-card" onClick={()=>DeletePieceOfWork(work)}>Delete</button>
